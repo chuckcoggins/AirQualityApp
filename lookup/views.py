51f2c199386088318284f5693c089e6c6d2dfe74
Chuck <chuck.coggins@gmail.com>
2020-12-30 22:07:20 -0500

    updated home.html

--- lookup/views.py
+++ lookup/views.py
@@ -2,8 +2,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(requests):
-    return render(requests, 'home.html', {})
+def home(request):
+    import json
+    import requests
 
-def about(requests):
-    return render(requests, 'about.html', {})
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=48059&distance=100&API_KEY=146057C6-45D9-4C56-BF86-253216A93628")
+
+    try:
+        api = json.loads(api_request.content)   # call json and load up the content from api_request
+
+    except Exception as e:
+        api = "Error..."
+
+
+    return render(request, 'home.html', {'api': api})
+
+def about(request):
+    return render(request, 'about.html', {})
